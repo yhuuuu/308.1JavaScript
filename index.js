1a7676928e4ddb071960e31534a4005827eb319a
Yuting Hu <yuting.hu.careers@gmail.com>
2024-01-11 14:01:31 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*   Data Manipulation in JavaScript
+
+Create variable declarations using both let and const.
+Apply different types of data literals.
+Use arithmetic operators to manipulate data.
+Use comparison operators to compare data.
+Perform string concatenations.
+Implement escape sequences in strings for special characters.
+Use template literals for string interpolation and multi-line strings.
+Create effective documentation through the use of comments. 
+*/
