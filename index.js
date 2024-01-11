2b9938d94a8db41d235af6d664fd17e976fa0be7
Yuting Hu <yuting.hu.careers@gmail.com>
2024-01-11 14:39:14 -0800

    Part1 completed

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-/*   Data Manipulation in JavaScript
+/*   Data Manipulation in JavaScript*/
 
-Create variable declarations using both let and const.
-Apply different types of data literals.
-Use arithmetic operators to manipulate data.
-Use comparison operators to compare data.
-Perform string concatenations.
-Implement escape sequences in strings for special characters.
-Use template literals for string interpolation and multi-line strings.
-Create effective documentation through the use of comments. 
-*/
+// Part 1: Math Problem
+
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+
+// Finally, log the results.
+// console.log(isValid);
+
+// Here's another example of how this COULD be done,
+// but it SHOULD NOT be done this way. As programmers,
+// we break things into small, manageable pieces so that
+// they can be better understood, scaled, and maintained.
+// const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+//   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
+//   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
+//   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  //Check if all numbers are deivisible by 5.
+  const allDeivisibleBy5 = (n1%5) +(n2%5)+(n3%5)+(n4%5)==0
+  console.log(`All number are divisible by 5: ${allDeivisibleBy5}`)
+
+  //Check if the first number is larger than last
+  const fisrtNumLargerThanLast = n1>n4
+  console.log(`First number is larger than last: ${fisrtNumLargerThanLast}`)
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+const firstNumSubtractSecNum = n2-n1
+
+// Multiply the result by the third number.
+const result = firstNumSubtractSecNum * n3
+
+// Find the remainder of dividing the result by the fourth number.
+const remainder = result%n4
+console.log(`The remainder of dividing the result by the fourth number is ${remainder}`)
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate
+const isLess25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+console.log(`No value is greader than 15:${isLess25} `)
